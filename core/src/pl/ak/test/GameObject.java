21ff3132d68bf05dab0064a8b80d7413787bde17
kemp4 <kempa44@gmail.com>
2016-09-21 15:40:15 +0200

    added input and textures

--- core/src/pl/ak/test/GameObject.java
+++ core/src/pl/ak/test/GameObject.java
@@ -1,35 +1,49 @@
 package pl.ak.test;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameObject {
-
+	private Sprite sprajt;
 	private Texture tekstura;
 	private Vector2 polozenie;
-	
-	
-	
+	private Vector2 predkosc;
+	private boolean trwaZmianaKierunku;
+	private float stalazmiany=0.5f;
+
 	public GameObject (Texture tekst){
 		tekstura=tekst;
+		setSprajt(new Sprite(tekstura));
+		predkosc=new Vector2(0,0);
 		setPolozenie(new Vector2(200,200));
-		
+		trwaZmianaKierunku=false;
 	}
-	
+
 	public void ZmienPolozenie(int dx, int dy){
 		polozenie.x+=dx;
 		polozenie.y+=dy;
+		sprajt.setPosition(polozenie.x,polozenie.y);
+
 	}
-	 public boolean czyWSrodku(float h, float w){
-		 if ((polozenie.x<0) || (polozenie.y<0) || polozenie.x>(w-tekstura.getWidth()) || polozenie.y>(h-tekstura.getHeight())){
+	public void ZmienPolozenie(float dx, float dy){
+		polozenie.x+=dx;
+		polozenie.y+=dy;
+		sprajt.setPosition(polozenie.x,polozenie.y);
+
+	}
+	public boolean czyWSrodku(float h, float w){
+		if ((polozenie.x<0) || (polozenie.y<0) || polozenie.x>(w-tekstura.getWidth()) || polozenie.y>(h-tekstura.getHeight())){
 			return false; 
-		 }
+		}
 		return true;
-	 }
+	}
+
+
 	
 
-	 
-	 
+
 	public Texture getTekstura() {
 		return tekstura;
 	}
@@ -42,5 +56,53 @@ public class GameObject {
 		this.polozenie = polozenie;
 	}
 
+	public Sprite getSprajt() {
+		return sprajt;
+	}
 
+	public void setSprajt(Sprite sprajt) {
+		this.sprajt = sprajt;
+	}
+
+	public void ZmienKierunek() {
+		trwaZmianaKierunku=true;
+			if (sprajt.getScaleX()>0)
+			{
+				stalazmiany=-0.5f;
+			}else{
+				stalazmiany=0.5f;
+			}
+	}
+
+	public void ogranijRybke() {
+		if (trwaZmianaKierunku){
+			if (Math.abs(sprajt.getScaleX())<=1){
+				sprajt.scale(stalazmiany*Gdx.graphics.getDeltaTime());
+				sprajt.setScale(sprajt.getScaleX(), 1.0f);
+			}
+			else
+			{				
+				sprajt.setScale(Math.round(sprajt.getScaleX()),1.0f);
+				trwaZmianaKierunku=false;
+			}
+		}
+		aktualizujPolozenie();
+		
+	}
+
+	private void aktualizujPolozenie() {
+		
+		polozenie.add(predkosc);
+		sprajt.setPosition(polozenie.x, polozenie.y);
+		if (!czyWSrodku(799, 604)){
+			predkosc.x=-predkosc.x;
+			predkosc.y=-predkosc.y;
+		}
+	}
+
+	public void ZmienPredkosc(float x, float y) {
+		predkosc.x+=x;
+		predkosc.y+=y;
+		
+	}
 }
--- core/src/pl/ak/test/Test.java
+++ core/src/pl/ak/test/Test.java
@@ -3,9 +3,7 @@ package pl.ak.test;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -16,7 +14,7 @@ public class Test extends ApplicationAdapter {
 	GameObject gracz;
 	float w;
 	float h;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -24,54 +22,82 @@ public class Test extends ApplicationAdapter {
 		img = new Texture("Ryb3.png");
 		gracz = new GameObject(img);
 		w = Gdx.graphics.getWidth();
-        h = Gdx.graphics.getHeight();
+		h = Gdx.graphics.getHeight();
 
 	}
 
 	@Override
 	public void render () {
 		update();
-		
+
 		Gdx.gl.glClearColor(1, 1, 0.7f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		
+
 		batch.draw(ocean,0,0);
+		gracz.getSprajt().draw(batch);
+
+
 		batch.draw(gracz.getTekstura(),gracz.getPolozenie().x,gracz.getPolozenie().y);
-		
+
+
 		batch.end();
 	}
-	
+
 	private void update() {
-		if(Gdx.input.isKeyPressed(Keys.W)){
-			gracz.ZmienPolozenie(0, 1);
-			if (!gracz.czyWSrodku(h, w)){
-				gracz.ZmienPolozenie(0, -1);
-			}
-		}
-		if(Gdx.input.isKeyPressed(Keys.S)){
-			gracz.ZmienPolozenie(0, -1);
-			if (!gracz.czyWSrodku(h, w)){
-				gracz.ZmienPolozenie(0, 1);
-			}
-		}
-		if(Gdx.input.isKeyPressed(Keys.A)){
-			gracz.ZmienPolozenie(-1, 0);
-			if (!gracz.czyWSrodku(h, w)){
-				gracz.ZmienPolozenie(1, 0);
-			}
-		}
-		if(Gdx.input.isKeyPressed(Keys.D)){
-			gracz.ZmienPolozenie(1, 0);
-			if (!gracz.czyWSrodku(h, w)){
-				gracz.ZmienPolozenie(-1, 0);
-			}
-		}
+
+		ogarnijKlawisze();
+		gracz.ogranijRybke();
 	}
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	private void ogarnijKlawisze() {
+		if(Gdx.input.isKeyPressed(Keys.W)){
+			gracz.ZmienPredkosc(0.0f,1.0f);
+
+		}
+		if(Gdx.input.isKeyPressed(Keys.S)){
+			gracz.ZmienPredkosc(0.0f,-1.0f);
+
+		}
+		if(Gdx.input.isKeyPressed(Keys.A)){
+			gracz.ZmienPredkosc(-1.0f,0.0f);
+
+		}
+		if(Gdx.input.isKeyPressed(Keys.D)){
+			gracz.ZmienPredkosc(1.0f,0.0f);
+
+		}
+		if(Gdx.input.isKeyPressed(Keys.SPACE)){
+			gracz.ZmienKierunek();
+			if(Gdx.input.isKeyPressed(Keys.W)){
+				gracz.ZmienPolozenie(0, 1);
+				if (!gracz.czyWSrodku(h, w)){
+					gracz.ZmienPolozenie(0, -1);
+				}
+			}
+			if(Gdx.input.isKeyPressed(Keys.S)){
+				gracz.ZmienPolozenie(0, -1);
+				if (!gracz.czyWSrodku(h, w)){
+					gracz.ZmienPolozenie(0, 1);
+				}
+			}
+			if(Gdx.input.isKeyPressed(Keys.A)){
+				gracz.ZmienPolozenie(-1, 0);
+				if (!gracz.czyWSrodku(h, w)){
+					gracz.ZmienPolozenie(1, 0);
+				}
+			}
+			if(Gdx.input.isKeyPressed(Keys.D)){
+				gracz.ZmienPolozenie(1, 0);
+				if (!gracz.czyWSrodku(h, w)){
+					gracz.ZmienPolozenie(-1, 0);
+				}
+			}
+		}
 	}
-}
+		@Override
+		public void dispose () {
+			batch.dispose();
+			img.dispose();
+		}
+	}
